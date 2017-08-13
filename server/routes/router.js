8290b0a957304600c6f04324fa624bfa501125f2
nick-t-go <nickt444@gmail.com>
2017-08-13 17:57:54 -0400

    fixed router

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,44 +4,55 @@ const express = require("express");
 const yelp = require('yelp-fusion');
 const app = express.Router();
 
-const clientId = "MH4ELBb7gGsONQjHxktnmg"
-const clientSecret = "2ZcOFByhjVMBl9FkMGWz63TbFsvBDEyIcitXbtNiKhYOaxZYPwTsXDZQKuicQmxA"
+const clientId = "MH4ELBb7gGsONQjHxktnmg";
+const clientSecret = "2ZcOFByhjVMBl9FkMGWz63TbFsvBDEyIcitXbtNiKhYOaxZYPwTsXDZQKuicQmxA";
 
-const token = yelp.accessToken(clientId, clientSecret)
-    .then(function(response) {
-        return response.jsonBody.access_token
-    }).catch(e => {
-        console.log("error", e);
-    });
-console.log(token)
+function getYelpToken() {
+    return yelp.accessToken(clientId, clientSecret)
+        .then(response => {
+            return response.jsonBody.access_token;
+        });
+}
+
+app.use('/', (req, res, next) => {
+    const token = app.get('YELP_TOKEN');
+
+    if (token) {
+        req.yelpToken = token;
+        next();
+    } else {
+        getYelpToken()
+            .then(t => {
+                app.set('YELP_TOKEN', t);
+                req.yelpToken = t;
+                next();
+            })
+            .catch(next);
+    }
+});
 
 app.get("/autoComplete", function(req, res) {
-    token.then(function(toke) {
-        console.log(req)
-        client = yelp.client(toke);
-        client.autocomplete({
-            text: 'pizza'
-        }).then(response => {
-            console.log(response.jsonBody.terms[0].text);
-        }).catch(e => {
-            console.log(e);
-        });
-    })
-})
+    const client = yelp.client(req.yelpToken);
+    client.autocomplete({
+        text: 'pizza'
+    }).then(response => {
+        console.log(response.jsonBody.terms[0].text);
+    }).catch(e => {
+        console.log(e);
+    });
+});
 
 app.get("/search", function(req, res) {
-    let client;
-    token.then(function(toke) {
-        client = yelp.client(toke);
-        client.search({
-            term: 'Four Barrel Coffee',
-            location: 'san francisco, ca'
-        }).then(response => {
-            res.send(response.jsonBody)
-        }).catch(e => {
-            console.log(e)
-        })
-    })
-})
+    const client = yelp.client(req.token);
+
+    client.search({
+        term: 'Four Barrel Coffee',
+        location: 'san francisco, ca'
+    }).then(response => {
+        res.send(response.jsonBody);
+    }).catch(e => {
+        console.log(e);
+    });
+});
 
 module.exports = app;
